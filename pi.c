ee58e0f9cade3dc5c8673a02fbb864dd2d810e3b
jaceg22 <gandhijace@gmail.com>
2025-03-04 16:31:16 -0500

    Update pi.c

--- pi.c
+++ pi.c
@@ -3,75 +3,53 @@
 #include <pthread.h>
 #include <math.h>
 
-#define RAND_MAX_DOUBLE ((double)RAND_MAX + 1)
-
-long total_darts, darts_per_thread;
-long points_in_circle = 0;
+#define RAND_MAX_DOUBLE ((double)RAND_MAX+1)
+long totalDarts, darts_per_thread;
+long circle_points=0;
 pthread_mutex_t lock;
-
-// Function to generate a random double in range [0,1]
-double random_double() {
-    return rand() / RAND_MAX_DOUBLE;
+double random_double(){
+    return rand()/RAND_MAX_DOUBLE;
 }
-
-// Thread function to throw darts
-void* throw_darts(void* arg) {
-    long count = 0;
-    for (long i = 0; i < darts_per_thread; i++) {
-        double x = random_double() * 2.0 - 1.0;
-        double y = random_double() * 2.0 - 1.0;
-        
-        if ((x * x + y * y) <= 1.0) {  // Check if inside circle
+void* throw_darts(void* arg){
+    long count=0;
+    for (long i=0; i<darts_per_thread; i++){
+        double x=random_double()*2.0-1.0;
+        double y=random_double()*2.0-1.0;
+        if ((x*x+y*y)<=1.0){
             count++;
         }
     }
-
-    // Update global count with thread safety
     pthread_mutex_lock(&lock);
-    points_in_circle += count;
+    circle_points+=count;
     pthread_mutex_unlock(&lock);
-    
     return NULL;
 }
-
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
+int main(int argc, char* argv[]){
+    if (argc!=3){
         fprintf(stderr, "usage: pi <NUMBER_OF_DARTS> <NUMBER_OF_THREADS>\n");
         return 1;
     }
-
-    total_darts = atol(argv[1]);
-    int num_threads = atoi(argv[2]);
-
-    if (total_darts < 5000000) {
+    totalDarts=atol(argv[1]);
+    int numThreads=atoi(argv[2]);
+    if (totalDarts<5000000){
         fprintf(stderr, "The number of darts must be >= 5000000\n");
         return 1;
     }
-
-    if (num_threads < 2) {
+    if (numThreads<2){
         fprintf(stderr, "The number of threads must be >= 2\n");
         return 1;
     }
-
-    pthread_t threads[num_threads];
-    darts_per_thread = total_darts / num_threads;
+    pthread_t threads[numThreads];
+    darts_per_thread=totalDarts/numThreads;
     pthread_mutex_init(&lock, NULL);
-
-    // Create threads
-    for (int i = 0; i < num_threads; i++) {
+    for (int i=0; i<numThreads; i++){
         pthread_create(&threads[i], NULL, throw_darts, NULL);
     }
-
-    // Join threads
-    for (int i = 0; i < num_threads; i++) {
+    for (int i=0; i<numThreads; i++){
         pthread_join(threads[i], NULL);
     }
-
     pthread_mutex_destroy(&lock);
-
-    // Estimate and print π
-    double pi = 4.0 * points_in_circle / total_darts;
-    printf("Pi = %f\n", pi);
-
+    double pi=4.0*circle_points/totalDarts;
+    printf("Pi=%f\n", pi);
     return 0;
 }
